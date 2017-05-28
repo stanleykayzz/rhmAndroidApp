6d0b1dee96be7339cd68ccbc661682ba3d7c10e3
Alvin Ondzounga <alvin.ondzounga@avanade.com>
2017-05-28 17:07:38 +0200

    ajout du code dans les fragments et dans le mainActivity pour pouvoir afficher le contenu des fragment en fonction du menu selectionné

--- app/src/main/java/com/example/rhm/rhmandroidapp/MainActivity.java
+++ app/src/main/java/com/example/rhm/rhmandroidapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.rhm.rhmandroidapp;
 
+import android.app.FragmentManager;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +14,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import rhm.com.fragment.gallery;
+import rhm.com.fragment.main;
+import rhm.com.fragment.booking;
+import rhm.com.fragment.room;
+import rhm.com.fragment.hotel;
+import rhm.com.fragment.login;
+import rhm.com.fragment.logout;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +49,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //On rajoute du code pour gérer nos fragments
+        FragmentManager fragMan = getFragmentManager();
+        fragMan.beginTransaction().replace(R.id.content_frame, new main()).commit();
     }
 
     @Override
@@ -77,22 +90,27 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+        FragmentManager fragMan = getFragmentManager();
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.content_frame) {
+            fragMan.beginTransaction().replace(R.id.content_frame, new main()).commit();
         } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+            fragMan.beginTransaction().replace(R.id.content_frame, new gallery()).commit();
+        } else if (id == R.id.nav_book) {
+            fragMan.beginTransaction().replace(R.id.content_frame, new booking()).commit();
+        } else if (id == R.id.nav_room) {
+            fragMan.beginTransaction().replace(R.id.content_frame, new room()).commit();
+        }else if (id == R.id.nav_hotel) {
+            fragMan.beginTransaction().replace(R.id.content_frame, new hotel()).commit();
+        } else if (id == R.id.nav_login) {
+            fragMan.beginTransaction().replace(R.id.content_frame, new login()).commit();
+        } else if (id == R.id.nav_logout) {
+            fragMan.beginTransaction().replace(R.id.content_frame, new logout()).commit();
+        } /*else if (id == R.id.nav_send) {
+            fragMan.beginTransaction().replace(R.id.content_frame, new main_fragment()).commit();
+        }*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
--- app/src/main/java/rhm/com/fragment/main.java
+++ app/src/main/java/rhm/com/fragment/main.java
@@ -1,29 +1,30 @@
 package rhm.com.fragment;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.rhm.rhmandroidapp.R;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link login.OnFragmentInteractionListener} interface
+ * {@link main.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link login#newInstance} factory method to
+ * Use the {@link main#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class login extends Fragment {
+
+public class main extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
