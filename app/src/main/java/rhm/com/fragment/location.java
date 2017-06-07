eb6e6ff9b53a2148ea0101749d9d374af042cf03
Alvin Ondzounga <alvin.ondzounga@avanade.com>
2017-06-07 16:01:35 +0200

    Modification des bibliothèque de fragment et de manager de toutes les classes de fragments, création et configuration de la googlemap dans le fragment location

--- app/src/main/java/rhm/com/fragment/location.java
+++ app/src/main/java/rhm/com/fragment/location.java
@@ -3,12 +3,21 @@ package rhm.com.fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.app.FragmentManager;
+
 
 import com.example.rhm.rhmandroidapp.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,92 +27,46 @@ import com.example.rhm.rhmandroidapp.R;
  * Use the {@link location#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class location extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class location extends Fragment implements OnMapReadyCallback {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    private GoogleMap myGoogleMap;
 
     public location() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment location.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static location newInstance(String param1, String param2) {
-        location fragment = new location();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        View fragLocation = inflater.inflate(R.layout.fragment_location, container, false);
+        return fragLocation;
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment supportMapFrag = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.myMap);
+        supportMapFrag.getMapAsync(this);
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    public void onMapReady(GoogleMap googleMap) {
+        this.myGoogleMap = googleMap;
+        //on déclare les coordonnées géographique de la zone que l'ont veut marquer ( Seoul Koré du sud pour les test )
+        LatLng coord = new LatLng(37.566535, 126.97796919999996);
+        MarkerOptions options = new MarkerOptions();
+        options.position(coord).title("L'endroit que vous cherchez certainement").snippet("Avec de petites explications en plus");
+        this.myGoogleMap.addMarker(options);
+        //on zoom la caméra sur le point de destination
+        this.myGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coord,13));
+        /*this.myGoogleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        this.myGoogleMap.getUiSettings().setAllGesturesEnabled(true);
+        this.myGoogleMap.getUiSettings().setCompassEnabled(true);
+        this.myGoogleMap.getUiSettings().setZoomControlsEnabled(true);*/
     }
 }
