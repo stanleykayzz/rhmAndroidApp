e22d421485dab6900d01e2a595d483e5b692a3ef
Alvin Ondzounga <alvin.ondzounga@avanade.com>
2017-06-12 02:04:05 +0200

    Ajout de la textView et de l'évenement sur click qui permet d'afficher le fragment d'inscription lorsqu'on click dessus

--- app/src/main/java/rhm/com/fragment/login.java
+++ app/src/main/java/rhm/com/fragment/login.java
@@ -4,11 +4,15 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.rhm.rhmandroidapp.R;
+import com.example.rhm.rhmandroidapp.suscribe;
 
 
 /**
@@ -25,7 +29,17 @@ public class login extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_login, container, false);
+        TextView forgotPassword = (TextView)v.findViewById(R.id.forgotPassword);
         getActivity().setTitle("Connexion");
+
+        forgotPassword.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //new Toast(getActivity()).makeText(getActivity(), "Button1 clicked.", Toast.LENGTH_SHORT).show();
+                FragmentManager fragMan = getFragmentManager();
+                fragMan.beginTransaction().replace(R.id.content_frame, new suscribe()).commit();
+            }
+        });
 
         return v;
 
