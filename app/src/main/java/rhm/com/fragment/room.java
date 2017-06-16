9560592e2a6cea103b24d163fcd219a04cace288
Alvin Ondzounga <alvin.ondzounga@avanade.com>
2017-06-16 16:08:00 +0200

    Ajout d'un intemclickListener et envoie des données vers la nouvelle page

--- app/src/main/java/rhm/com/fragment/room.java
+++ app/src/main/java/rhm/com/fragment/room.java
@@ -1,39 +1,36 @@
 package rhm.com.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
-
+import android.widget.Toast;
+import com.example.rhm.rhmandroidapp.MainActivity;
 import com.example.rhm.rhmandroidapp.R;
-
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
 import rhm.com.adapters.RoomListAdapter;
 import rhm.com.entities.Chambre;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link room.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link room#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class room extends Fragment {
 
     private ListView listeDesChambres;
     private List<Chambre> chambres;
 
+    public room(){
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         this.chambres = new ArrayList<Chambre>();
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_room, container, false);
@@ -43,6 +40,24 @@ public class room extends Fragment {
         chambres.add(new Chambre(4,"Suite Exécutive", 200000,R.drawable.room10  ));
         this.listeDesChambres = (ListView) v.findViewById(R.id.listViewRooms);
         this.listeDesChambres.setAdapter(new RoomListAdapter(getContext(), chambres));
+        //on ajoute le code pour afficher la page de détails lorsqu'on clique sur un objet
+        this.listeDesChambres.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Chambre detailRoom = chambres.get(position);
+
+                DetailedRoom dr = new DetailedRoom();
+                Bundle bundle = new Bundle();
+
+                bundle.putString("LePrix",detailRoom.getPriceIntoString());
+                bundle.putString("LeType",detailRoom.getTypeRoom());
+
+                dr.setArguments(bundle);
+
+                FragmentManager fragMan = getFragmentManager();
+                fragMan.beginTransaction().replace(R.id.content_frame, dr).commit();
+            }
+        });
         getActivity().setTitle("Nos chambres");
         return v;
     }
